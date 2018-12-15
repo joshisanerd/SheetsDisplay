fe22cdbc8922796edaa394361dca5854632b2756
Josh Hunt <joshhruns@gmail.com>
2018-12-14 20:34:09 -0500

    Initial commit

---
+++ GoogleSheets/Program.cs
@@ -0,0 +1,99 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace GoogleSheets
+{
+    class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/sheets.googleapis.com-dotnet-quickstart.json
+        static string[] Scopes = { SheetsService.Scope.SpreadsheetsReadonly };
+        static string ApplicationName = "Google Sheets API .NET Quickstart";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Sheets API service.
+            var service = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+            
+            try
+            {
+                // Define request parameters.
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("Spreadsheet ID: ");
+                Console.ResetColor();
+                String spreadsheetId = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("Range: ");
+                Console.ResetColor();
+                String range = Console.ReadLine();
+                SpreadsheetsResource.ValuesResource.GetRequest request =
+                        service.Spreadsheets.Values.Get(spreadsheetId, range);
+
+                // Prints the names and majors of students in a sample spreadsheet:
+                // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+                ValueRange response = request.Execute();
+                IList<IList<Object>> values = response.Values;
+                if (values != null && values.Count > 0)
+                {
+                    int i = 0;
+                    foreach (var row in values)
+                    {
+                        if (i == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.WriteLine("{0}, {1}", row[0], row[1]);
+                            Console.ResetColor();
+                        }
+                        else
+                        {
+                            // Print columns A and E, which correspond to indices 0 and 4.
+                            Console.WriteLine("{0}, {1}", row[0], row[1]);
+                        }
+                        i++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found.");
+                }
+                Console.Read();
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ResetColor();
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
